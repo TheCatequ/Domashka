4345db25c4fffe4d5b8fda14332ecefdefce2e54
TheCatequ <78447511+TheCatequ@users.noreply.github.com>
2021-02-03 00:09:59 +0300

    Update domashka.js

--- domashka.js
+++ domashka.js
@@ -1,22 +1,22 @@
-let button = document.querySelector('#id1')
+let button1 = document.querySelector('#id1')
 
-let button1= document.querySelector('#id2')
+let button2= document.querySelector('#id2')
 
-let link=[ 'https://www.twitch.tv',
+let cat=[ 'https://www.twitch.tv',
             'https://www.youtube.com/',
             'https://www.hltv.org',
             'https://www.faceit.com/ru/pubg'
 
 ]
 
-function random(n,x){
-    return Math.floor(Math.random()*(x-n)+n)
+function random(a,b){
+    return Math.floor(Math.random()*(a-b)+a)
 }
 
-button.addEventListener('click',function(){
-    window.location.href = link[random(0,4)]
+button1.addEventListener('click',function(){
+    window.location.href = cat[random(0,4)]
 })
 
-button1.addEventListener('click', function(){ 
+button2.addEventListener('click', function(){ 
 document.body.style.backgroundColor = '#800080'
 })
